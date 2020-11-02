7f251b0609032f8212a1af5c170efe12cea62bdc
Martin Gazquez <lechu@SALDIVIA-DOMAIN.LAN>
2020-11-01 21:29:53 -0300

    save small change

---
+++ sub.js
@@ -0,0 +1,8 @@
+var inputName = (document.getElementById (full-name));
+var errorMessage = (document.getElementsByClassName ("error"));
+
+function Name (event) {
+    var value = event.target.value;
+
+    if (value.length < 6) 
+}
