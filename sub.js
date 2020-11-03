ad086ab920fc4cbb0aea3d6618737f231cfea71a
Martin Gazquez <lechu@SALDIVIA-DOMAIN.LAN>
2020-11-02 23:34:42 -0300

    end mail validate

--- sub.js
+++ sub.js
@@ -1,8 +1,31 @@
-var inputName = (document.getElementById (full-name));
-var errorMessage = (document.getElementsByClassName ("error"));
+var inputName = document.getElementById ("full-name");
+var errorMessage = document.getElementsByClassName ("error-message");
 
 function Name (event) {
-    var value = event.target.value;
+    var comparationfullname = event.target.value;
+       
+    if ((comparationfullname.length < 6)||(!comparationfullname.includes(" "))) {
+       errorMessage[0].style.display = "block";     
+    }
+     
+}
 
-    if (value.length < 6) 
+inputName.addEventListener ("blur", Name);
+
+function resetValidate (nameMessage) {
+     document.getElementById(nameMessage).style.display = "none";
+}
+ 
+function email () {
+    var email= document.getElementById("mailId").value;
+    
+  var a = email.indexOf("@");
+  var com = email.indexOf(".com");
+  var emailErrorMessage = document.getElementById("messageEmail");
+
+  if (a === -1 || com === -1) {
+    
+    emailErrorMessage.style.display = "block";
+    }    
+   
 }
