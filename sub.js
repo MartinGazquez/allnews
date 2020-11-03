983454b0f0a178bcad7213c7a2e75594276f0a76
Martin Gazquez <lechu@SALDIVIA-DOMAIN.LAN>
2020-11-03 16:55:28 -0300

    end password validation and address validation

--- sub.js
+++ sub.js
@@ -3,7 +3,7 @@
 var inputName = document.getElementById ("full-name");
 var errorMessage = document.getElementsByClassName ("error-message");
 
-function Name (event) {
+function name (event) {
     var comparationfullname = event.target.value;
        
     if ((comparationfullname.length < 6)||(!comparationfullname.includes(" "))) {
@@ -12,13 +12,14 @@ function Name (event) {
      
 }
 
-inputName.addEventListener ("blur", Name);
+inputName.addEventListener ("blur", name);
 
 function resetValidate (nameMessage) {
      document.getElementById(nameMessage).style.display = "none";
 }
  
 //Email Validation
+
 function email () {
   var email = document.getElementById("mailId").value;
     
@@ -34,28 +35,30 @@ function email () {
 }
 
 //Password Validation
-//<input id= "passwordId" onblur="password ()" onfocus="resetValidate ('messagePassword')" type="password" placeholder="Password">
-//<sub id="messagePassword" class="error-message">Al menos 8 caracteres, formados por letras y números</sub>
-function validate_password() {
+
+var lettersRegExp = /[a-z]/i;
+var numbersRegExp = /\d/g;
+
+function password() {
     var password = document.getElementById("passwordId").value;
     var passwordInvalid = document.getElementById("messagePassword");
-    var Letters = password.length;
+    var letters = password.length;
     var contentLetters = lettersRegExp.test(password);
     var contentNumbers = numbersRegExp.test(password);
     
   
-    if (Letters < 8 || !contentLetters || !contentNumbers) {
-      passwordInvalid.innerHTML = passwordIncorrect;
-      passwordInvalid.style.display = "block";
-      document.getElementById("password").classList.add("form-input-invalid");
-      return passwordIncorrect;
-    } else {
-      reset_validate("password", "messagePassword");
-      return true;
+    if (letters < 8 || !contentLetters || !contentNumbers) {
+     passwordInvalid.style.display = "block";
+
     }
   } 
 
+//Repeat Password Validation
+
+
+
 //Age Validation
+
 function age () {
     var validateAge = document.getElementById("ageId").value;
     
@@ -66,4 +69,23 @@ function age () {
     ageErrorMessage.style.display = "block";
                          }    
    
+}
+
+//Adress Validation
+
+function address () {
+    
+    var inputAdress = document.getElementById ("addressId").value;
+    var errorMessage = document.getElementById ("messageAddress");   
+    var contentLetters = lettersRegExp.test(inputAdress);
+    var contentNumbers = numbersRegExp.test(inputAdress);
+    var letters = inputAdress.length;
+    var space = inputAdress.indexOf (' ');
+    var firstWord = inputAdress.substring (0 , space);
+    var secondword = inputAdress.substring (space + 1);
+    
+    if (letters <5 || space === -1 || !contentLetters || !contentNumbers || firstWord.length === 0 || secondword.length === 0 ){
+       errorMessage.style.display = "block";     
+    }
+     
 }
